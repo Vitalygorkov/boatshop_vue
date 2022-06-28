e07c2587d346ba98c4363d7397fbb406a8a5f0ce
Vitalygorkov <vet55omsk@gmail.com>
2022-06-28 22:24:07 +0600

    contactform

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -12,9 +12,16 @@
 <input v-model="locality" type="text" name="city" ><br>
 <label for="fname">Email:</label><br>
 <input v-model="email" type="text" name="email" ><br>
-<p><textarea v-model="message" rows="10" cols="22" name="text"></textarea></p>
-<input v-on:click="click_submit()" type="submit">
+<label for="fname">Сообщение:</label><br>
+<textarea v-model="message" rows="10" cols="22" name="text"></textarea><br>
+<input v-on:click="checkform()" type="submit">
 </form>
+  <p v-if="errors.length">
+    <b>Пожалуйста исправьте указанные ошибки:</b>
+    <ul>
+      <li v-for="error in errors">{{ error }}</li>
+    </ul>
+  </p>
 <!-- n: {{frm_dat}} text -->
 </div>
 
@@ -26,17 +33,44 @@ export default {
     data() {
         return {
             showForm: true,
-            first_name: '',
+            errors: [],
+            first_name: null,
             last_name: "",
-            phone: "",
-            locality: "",
-            email: "",
+            phone: null,
+            locality: null,
+            email: null,
             message: "Напишите дополнительную информацию и пожелания к заказу",
 
         }
     },
     props: ['title', 'content'],
     methods: {
+        checkform(){
+            console.log('функция отправки начало')
+            if (this.first_name && this.phone && this.email && this.message){
+                console.log('функция иф отправки начало')
+                this.click_submit()
+                console.log('функция иф отправки окончание')
+                return true;
+            }
+
+            this.errors = [];
+
+            if (!this.first_name){
+                this.errors.push('Требуется указать имя.');
+            }
+            if (!this.phone){
+                this.errors.push('Требуется указать телефон.');
+            }
+            if (!this.email){
+                this.errors.push('Требуется указать email.');
+            } 
+            if (!this.message){
+                this.errors.push('Требуется указать комментарий к заказу.');
+            }
+            // this.$router.go(0);
+
+        },
         async click_submit(){
             let frm_dat = {
                 first_name: this.first_name,
@@ -56,7 +90,12 @@ export default {
             });
 
             let result = await response.json();
-            alert("Заявка успешно отправлена: " + result.message);
+            if(response.ok){
+                alert("Заявка успешно отправлена: " + result.message);
+            }else{
+                alert("Заявка НЕ отправлена, попробуйте обновить страницу : " + result.message);
+            }
+            
 
 
         }
